e11819d86db946c731c3457e33ad517136d0000f
BradNichol <bradleynichol@live.co.uk>
2020-02-15 16:06:47 +0000

    generateInitials function completed

--- challenges/week1.js
+++ challenges/week1.js
@@ -6,8 +6,9 @@ function capitalize(word) {
 function generateInitials(firstName, lastName) {
   if (firstName === undefined) throw new Error("firstName is required");
   if (lastName === undefined) throw new Error("lastName is required");
-  // Add your code here!
-}
+  
+  return firstName[0] + "." + lastName[0];
+};
 
 function addVAT(originalPrice, vatRate) {
   if (originalPrice === undefined) throw new Error("originalPrice is requied");
