e8f127cbc787813ec0cf799be9397af429125f1f
BradNichol <bradleynichol@live.co.uk>
2020-02-17 23:37:33 +0000

    eslint errors fixed (unnecessary semicolons

--- challenges/week1.js
+++ challenges/week1.js
@@ -1,14 +1,14 @@
 function capitalize(word) {
   if (word === undefined) throw new Error("word is required");
   return word[0].toUpperCase() + word.slice(1);
-};
+}
 
 function generateInitials(firstName, lastName) {
   if (firstName === undefined) throw new Error("firstName is required");
   if (lastName === undefined) throw new Error("lastName is required");
 
   return firstName[0] + "." + lastName[0];
-};
+}
 
 function addVAT(originalPrice, vatRate) {
   if (originalPrice === undefined) throw new Error("originalPrice is requied");
@@ -18,7 +18,7 @@ function addVAT(originalPrice, vatRate) {
   const toDecimal = vatRate / 100;
   
   return parseFloat(((originalPrice * toDecimal) + originalPrice).toFixed(2));
-};
+}
 
 function getSalePrice(originalPrice, reduction) {
   if (originalPrice === undefined) throw new Error("originalPrice is required");
@@ -29,7 +29,7 @@ function getSalePrice(originalPrice, reduction) {
 
   // parseFloat converts toFixed string back into number
   return parseFloat(salePrice.toFixed(2));
-};
+}
 
 function getMiddleCharacter(str) {
   if (str === undefined) throw new Error("str is required");
@@ -40,10 +40,10 @@ function getMiddleCharacter(str) {
   // for strings of even length
   if (str.length % 2 === 0) {
     return str[middleIndex - 1] + str[middleIndex];
-  };
+  }
   // for strings of odd length
   return str[middleIndex];
-};
+}
 
 function reverseWord(word) {
   if (word === undefined) throw new Error("word is required");
@@ -52,7 +52,7 @@ function reverseWord(word) {
   //and use join to turn back to a string
   return word.split("").reverse().join("");
 
-};
+}
 
 function reverseAllWords(words) {
   if (words === undefined) throw new Error("words is required");
@@ -63,7 +63,7 @@ function reverseAllWords(words) {
   });
 
   return newArray;
-};
+}
 
 
 function countLinuxUsers(users) {
@@ -73,10 +73,10 @@ function countLinuxUsers(users) {
   users.forEach((u) => {
     if (u.type === 'Linux') {
       count++;
-    };
+    }
   });
   return count;
-};
+}
 
 function getMeanScore(scores) {
   if (scores === undefined) throw new Error("scores is required");
@@ -86,7 +86,7 @@ function getMeanScore(scores) {
     total = total + n;
   });
   return parseFloat((total / scores.length).toFixed(2));
-};
+}
 
 
 function simpleFizzBuzz(n) {
@@ -101,8 +101,8 @@ function simpleFizzBuzz(n) {
     return 'buzz';
   } else {
     return n;
-  };
-};
+  }
+}
 
 module.exports = {
   capitalize,
