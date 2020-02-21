f190c23dbd67d40a764d2f214c9fbb5279cca0e6
BradNichol <bradleynichol@live.co.uk>
2020-02-21 19:00:32 +0000

    eslint & format corrections

--- challenges/week2.js
+++ challenges/week2.js
@@ -1,12 +1,12 @@
 function getFillings(sandwich) {
   if (sandwich === undefined) throw new Error("ingredients is required");
-  
+
   return sandwich.fillings;
 }
 
 function isFromManchester(person) {
   if (person === undefined) throw new Error("person is required");
-  
+
   if (person.city === 'Manchester') {
     return true;
   }
@@ -15,7 +15,7 @@ function isFromManchester(person) {
 
 function getBusNumbers(people) {
   if (people === undefined) throw new Error("people is required");
-  
+
   const busCapacity = 40;
   // Math.ceil rounds up to nearest whole integer
   return Math.ceil(people / busCapacity);
@@ -24,22 +24,22 @@ function getBusNumbers(people) {
 
 function countSheep(arr) {
   if (arr === undefined) throw new Error("arr is required");
-  
+
   let count = 0;
   arr.forEach((i) => {
-    if (i === 'sheep'){
+    if (i === 'sheep') {
       count++
-    };
+    }
   });
   return count;
 }
 
 function hasMPostCode(person) {
   if (person === undefined) throw new Error("person is required");
-  
-  if (person.address.postCode.startsWith('M') && person.address.city === 'Manchester'){
+
+  if (person.address.postCode.startsWith('M') && person.address.city === 'Manchester') {
     return true;
-  };
+  }
   return false;
 }
 
