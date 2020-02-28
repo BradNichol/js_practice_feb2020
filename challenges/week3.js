a9cfaa31f14e842ef9c4f03ade710be91263415e
BradNichol <bradleynichol@live.co.uk>
2020-02-28 18:27:41 +0000

    format and eslint updates

--- challenges/week3.js
+++ challenges/week3.js
@@ -1,6 +1,6 @@
 function getSquares(nums) {
   if (nums === undefined) throw new Error("nums is required");
-  
+
   return nums.map(n => n * n);
 }
 
@@ -14,13 +14,13 @@ function camelCaseWords(words) {
     // thereafter + concat rest of word with slice. Push into new array.
     newArr.push(words[i][0].toUpperCase() + words[i].slice(1))
   }
-  
+
   return newArr.join("")
 }
 
 function getTotalSubjects(people) {
   if (people === undefined) throw new Error("people is required");
-  
+
   let count = 0;
   people.forEach((i) => {
     // using dot notation to access object at: 
@@ -33,7 +33,7 @@ function getTotalSubjects(people) {
 function checkIngredients(menu, ingredient) {
   if (menu === undefined) throw new Error("menu is required");
   if (!ingredient) throw new Error("ingredient is required");
-  
+
   // NOTE - initially tried forEach to loop. Failed because:
   // 'There is no way to stop or break a forEach() loop' from MDN
   // for loop breaks Ok so used this instead.
@@ -49,15 +49,15 @@ function checkIngredients(menu, ingredient) {
 function duplicateNumbers(arr1, arr2) {
   if (arr1 === undefined) throw new Error("arr1 is required");
   if (arr2 === undefined) throw new Error("arr2 is required");
-  
+
   const arr3 = [];
   arr1.forEach((n1) => {
     arr2.forEach((n2) => {
       if (n1 === n2) {
-        arr3.push(n1)
+        arr3.push(n1);
       }
-    })
-  })
+    });
+  });
 
   // set can be used to return unique values only
   return [...new Set(arr3)].sort()
