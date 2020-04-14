01ef20c1b3a19312185a645f17aeaeb0a9fcdd97
Bradley Nichol <bradleynichol@live.co.uk>
2020-04-14 18:41:37 +0100

    sumMultiples test created

--- test/week9.test.js
+++ test/week9.test.js
@@ -6,3 +6,9 @@ const {
     createMatrix,
     areWeCovered
   } = require("../challenges/week9");
+
+  describe("sumMultiples", () => {
+    test("return the sum of any numbers which are a multiple of 3 or 5", () => {
+        expect(sumMultiples([1, 3, 5])).toBe(8);
+    });
+  });
