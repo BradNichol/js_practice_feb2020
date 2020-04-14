f6cf11b8bd739413cf4938cfc0f90794c06cd4b6
Bradley Nichol <bradleynichol@live.co.uk>
2020-04-14 20:01:59 +0100

    further tests added to sumMutliples

--- test/week9.test.js
+++ test/week9.test.js
@@ -11,4 +11,13 @@ const {
     test("return the sum of any numbers which are a multiple of 3 or 5", () => {
         expect(sumMultiples([1, 3, 5])).toBe(8);
     });
+
+    test("return 0 is no multiples of 3 or 5", () => {
+        expect(sumMultiples([1, 7, 8, 11])).toBe(0);
+    });
+
+    test(" Handles int and floats ok", () => {
+        expect(sumMultiples([1, 3.1, 5.2, 5.0, 10])).toBe(15);
+    });
+
   });
