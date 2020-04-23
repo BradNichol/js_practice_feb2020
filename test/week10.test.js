c0b43b7a1d49a625ac492383bf07ff30e79a1c4d
Bradley Nichol <bradleynichol@live.co.uk>
2020-04-23 15:30:55 +0100

    createRange test created, without step test yet

--- test/week10.test.js
+++ test/week10.test.js
@@ -10,5 +10,12 @@ describe("sumDigits", () => {
   test("Function takes a number and returns the sum of all its digits", () => {
     expect(sumDigits(235)).toBe(10);
   });
-
 });
+
+describe("createRange", () => {
+  test("Function creates an array of a range of numbers with a start and end number. Numbers are inclusive", () => {
+    expect(createRange(2, 6)).toEqual([2, 3, 4, 5, 6]);
+  });
+});
+
+
