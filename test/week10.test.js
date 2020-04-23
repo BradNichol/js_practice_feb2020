511143b1d9babdff95c1712828ab16a09c9d8508
Bradley Nichol <bradleynichol@live.co.uk>
2020-04-23 11:51:01 +0100

    sumDigits test created

--- test/week10.test.js
+++ test/week10.test.js
@@ -6,3 +6,9 @@ const {
   findWinner
 } = require("../challenges/week10");
 
+describe("sumDigits", () => {
+  test("Function takes a number and returns the sum of all its digits", () => {
+    expect(sumDigits(235)).toBe(10);
+  });
+
+});
