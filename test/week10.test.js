63f0cd713dfde78c21d99d4e0a967ab0d60256a9
Bradley Nichol <bradleynichol@live.co.uk>
2020-04-23 15:42:12 +0100

    test added to createRange for step number

--- test/week10.test.js
+++ test/week10.test.js
@@ -13,8 +13,12 @@ describe("sumDigits", () => {
 });
 
 describe("createRange", () => {
-  test("Function creates an array of a range of numbers with a start and end number. Numbers are inclusive", () => {
+  test("Uses a start and end numbers to a range of numbers in an array.", () => {
     expect(createRange(2, 6)).toEqual([2, 3, 4, 5, 6]);
+  });
+
+  test("Uses a start and end numbers to create range of numbers in array. Also uses a step number to provide gap between number sequence.", () => {
+    expect(createRange(3, 11)).toEqual([3, 5, 7, 9, 11]);
   });
 });
 
